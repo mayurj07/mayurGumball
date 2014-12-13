82a38da08b8f69a43e6e987e0d68909e3f9b1dfa
mayurj07 <mayurj07@gmail.com>
2014-12-13 06:13:20 +0000

    server deployed

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+
+/*
+ * GET home page.
+ */
+var MongoClient = require('mongodb').MongoClient;
+
+exports.index = function(req, res){
+		var userName="mayurnew";
+	var password="mayur";
+	MongoClient.connect("mongodb://"+userName+":"+password+"@ds045970.mongolab.com:45970/mayurdb", function(err, db) {
+		  if(!err) {
+		    console.log("We are connected");
+		    var collection=db.collection('gumball');
+		    collection.find({}).toArray(function(err,docs){
+		    	
+		    	var data =docs[0];
+		    	console.log(data);
+		    	res.writeHead(200,{"Content-Type":"application/json"});
+		    	res.end(JSON.stringify(data)+"\n");
+		    });
+		  }else{
+			  
+			  db.collection('gumball',function(err,collection){
+				  
+				  console.log(collection);
+			  });
+		  }
+		});
+};
+
+
+
